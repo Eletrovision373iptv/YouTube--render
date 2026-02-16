f02c012130d33fbd145fc4c659d53bc0bad43980
Eletrovision IPTV <75511527+Eletrovision373iptv@users.noreply.github.com>
2026-02-16 19:14:38 -0300

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
-# Instala dependências do node
 npm install
-# Baixa a versão estável do yt-dlp
 curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o yt-dlp
-# Dá permissão para o sistema rodar o arquivo
 chmod a+rx yt-dlp
